d2598f9c7b4bc02cf5ac8a2561b28a3a5c3e620f
hyeounguk2 <jim9823@naver.com>
2025-11-20 00:42:40 +0900

    divided service,controller and made ResponseEntity

--- shop/src/main/java/com/example/shop/controller/MemberController.java
+++ shop/src/main/java/com/example/shop/controller/MemberController.java
@@ -1,12 +1,13 @@
 package com.example.shop.controller;
 
+import com.example.shop.common.ResponseEntity;
 import com.example.shop.member.Member;
 import com.example.shop.member.MemberRepository;
 import com.example.shop.member.MemberRequest;
+import com.example.shop.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,33 +16,28 @@ import java.util.UUID;
 @RestController
 @RequestMapping("${api.v1}/members")
 public class MemberController {
-//    private final MemberRepository memberRepository;
-//
-//    public MemberController(MemberRepository memberRepository){
-//        this.memberRepository = memberRepository;
-//    }
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService){
+        this.memberService = memberService;
+    }
+
+    @Operation(
+            summary = "회원 목록 조회",
+            description = "public.member 테이블에 저장된 모든 회원을 조회한다"
+    )
+    @GetMapping
+    public ResponseEntity<List<Member>> findAll(){
+        return memberService.findAll();
+    }
+
     @Operation(
             summary = "회원 등록",
             description = "요청으로 받은 회원 정보를 public.member 테이블에 저장한다."
     )
     @PostMapping
-    public Member create(@RequestBody MemberRequest request){
-        Member member = new Member(
-                UUID.randomUUID(),
-                request.email(),
-                request.name(),
-                request.password(),
-                request.phone(),
-                request.saltKey(),
-                request.flag()
-        );
-        return memberRepository.save(member);
-//        Member member1 = memberRepository.save(member);
-//        int cnt = 0;
-//        if(member1 instanceof List){
-//            cnt = ((List<?>) member1).size();
-//        }else cnt=1;
-//        return new ResponseEntity<>(HttpStatus.OK.value(),member,cnt);
+    public ResponseEntity<Member> create(@RequestBody MemberRequest request){
+        return memberService.create(request);
     }
 
     @Operation(
@@ -49,36 +45,15 @@ public class MemberController {
             description = "요청으로 받은 회원 정보를 public.member 테이블에 수정한다."
     )
     @PutMapping("{id}")
-    public Member update(@RequestBody MemberRequest request, @PathVariable String id){
-        Member member = new Member(
-                id,
-                request.email(),
-                request.name(),
-                request.password(),
-                request.phone(),
-                request.saltKey(),
-                request.flag()
-        );
-        return memberRepository.save(member);
+    public ResponseEntity<Member> update(@RequestBody MemberRequest request, @PathVariable(name="id") String id){
+        return memberService.update(request,id);
     }
-
     @Operation(
             summary = "회원 정보 삭제",
             description = "요청으로 받은 회원 정보를 public.member 테이블에서 삭제한다."
     )
     @DeleteMapping("{id}")
-    public void update(@PathVariable String id){
-        memberRepository.deleteById(UUID.fromString(id));
-    }
-    @Autowired
-    private MemberRepository memberRepository;
-
-    @Operation(
-            summary = "회원 목록 조회",
-            description = "public.member 테이블에 저장된 모든 회원을 조회한다"
-    )
-    @GetMapping
-    public List<Member> findAll(){
-        return memberRepository.findAll();
+    public ResponseEntity<Void> delete(@PathVariable(name="id") String id){
+        return memberService.delete(id);
     }
 }
