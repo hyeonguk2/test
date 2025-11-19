f033a60b243b385b440b7ef3685f84d5d8e45f13
hyeounguk2 <jim9823@naver.com>
2025-11-19 17:00:39 +0900

    Initial commit

---
+++ shop/src/main/java/com/example/shop/controller/MemberController.java
@@ -0,0 +1,84 @@
+package com.example.shop.controller;
+
+import com.example.shop.member.Member;
+import com.example.shop.member.MemberRepository;
+import com.example.shop.member.MemberRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("${api.v1}/members")
+public class MemberController {
+//    private final MemberRepository memberRepository;
+//
+//    public MemberController(MemberRepository memberRepository){
+//        this.memberRepository = memberRepository;
+//    }
+    @Operation(
+            summary = "회원 등록",
+            description = "요청으로 받은 회원 정보를 public.member 테이블에 저장한다."
+    )
+    @PostMapping
+    public Member create(@RequestBody MemberRequest request){
+        Member member = new Member(
+                UUID.randomUUID(),
+                request.email(),
+                request.name(),
+                request.password(),
+                request.phone(),
+                request.saltKey(),
+                request.flag()
+        );
+        return memberRepository.save(member);
+//        Member member1 = memberRepository.save(member);
+//        int cnt = 0;
+//        if(member1 instanceof List){
+//            cnt = ((List<?>) member1).size();
+//        }else cnt=1;
+//        return new ResponseEntity<>(HttpStatus.OK.value(),member,cnt);
+    }
+
+    @Operation(
+            summary = "회원 수정",
+            description = "요청으로 받은 회원 정보를 public.member 테이블에 수정한다."
+    )
+    @PutMapping("{id}")
+    public Member update(@RequestBody MemberRequest request, @PathVariable String id){
+        Member member = new Member(
+                id,
+                request.email(),
+                request.name(),
+                request.password(),
+                request.phone(),
+                request.saltKey(),
+                request.flag()
+        );
+        return memberRepository.save(member);
+    }
+
+    @Operation(
+            summary = "회원 정보 삭제",
+            description = "요청으로 받은 회원 정보를 public.member 테이블에서 삭제한다."
+    )
+    @DeleteMapping("{id}")
+    public void update(@PathVariable String id){
+        memberRepository.deleteById(UUID.fromString(id));
+    }
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Operation(
+            summary = "회원 목록 조회",
+            description = "public.member 테이블에 저장된 모든 회원을 조회한다"
+    )
+    @GetMapping
+    public List<Member> findAll(){
+        return memberRepository.findAll();
+    }
+}
---
+++ shop/src/main/java/com/example/shop/member/MemberRequest.java
@@ -0,0 +1,10 @@
+package com.example.shop.member;
+public record MemberRequest(
+        String email,
+        String name,
+        String password,
+        String phone,
+        String saltKey,
+        String flag
+) {
+}
